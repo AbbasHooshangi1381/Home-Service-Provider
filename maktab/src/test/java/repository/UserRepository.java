87437ea51081a7600253383ac2bd7e89694907f4
abbas1381 <basjfryan@gmail.com>
2023-09-10 18:07:39 +0330

    add login method

--- maktab/src/test/java/repository/UserRepository.java
+++ maktab/src/test/java/repository/UserRepository.java
@@ -5,11 +5,12 @@ import model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserRepository{
-JdbcConnection jdbcConnection=new JdbcConnection();
-Connection connection= jdbcConnection.getConnection();
+public class UserRepository {
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
 
     public UserRepository() throws SQLException {
     }
@@ -19,12 +20,33 @@ Connection connection= jdbcConnection.getConnection();
         String add = "INSERT INTO users(firstname,lastname,username,password)VALUES(?,?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(add);
         preparedStatement.setString(1, user.getFirstname());
-        preparedStatement.setString(2,user.getLastname());
-        preparedStatement.setString(3,user.getPassword());
-        preparedStatement.setString(4,user.getUsername());
+        preparedStatement.setString(2, user.getLastname());
+        preparedStatement.setString(3, user.getPassword());
+        preparedStatement.setString(4, user.getUsername());
         int resault = preparedStatement.executeUpdate();
         return resault;
     }
 
+    public User login(String username) throws SQLException {
+        String loginQuery = "SELECT * FROM username WHERE username=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(loginQuery);
+        preparedStatement.setString(1, username);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            User user= new User(
+                    resultSet.getInt("id"),
+            resultSet.getString("firstname"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getString("lastname")
+       );
+            return user;
+
+
+        }
+        else
+            return null;
+
+    }
 
 }
