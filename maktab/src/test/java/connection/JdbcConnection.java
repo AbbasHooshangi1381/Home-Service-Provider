bb7e9442cff1066eb1620de778663d76bfe9f5ce
abbas1381 <basjfryan@gmail.com>
2023-09-10 13:44:05 +0330

    add connection meat

--- maktab/src/test/java/connection/JdbcConnection.java
+++ maktab/src/test/java/connection/JdbcConnection.java
@@ -1,4 +1,13 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    Connection connection = DriverManager.getConnection
+            ("jdbc:postgresql://localhost:5432/postgres","postgres","abbas1381.aa");
+
+    public JdbcConnection() throws SQLException {
+    }
 }
