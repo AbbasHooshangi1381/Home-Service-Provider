09d920adf3f810a8c237aedbcc2b97b8104227eb
abbas1381 <basjfryan@gmail.com>
2023-09-10 16:02:02 +0330

    add save method

--- maktab/src/test/java/repository/UserRepository.java
+++ maktab/src/test/java/repository/UserRepository.java
@@ -1,7 +1,30 @@
 package repository;
 
-public class UserRepository {
+import connection.JdbcConnection;
+import model.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserRepository{
+JdbcConnection jdbcConnection=new JdbcConnection();
+Connection connection= jdbcConnection.getConnection();
+
+    public UserRepository() throws SQLException {
+    }
+
+    public int save(User user) throws SQLException {
+
+        String add = "INSERT INTO users(firstname,lastname,username,password)VALUES(?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, user.getFirstname());
+        preparedStatement.setString(2,user.getLastname());
+        preparedStatement.setString(3,user.getPassword());
+        preparedStatement.setString(4,user.getUsername());
+        int resault = preparedStatement.executeUpdate();
+        return resault;
+    }
 
 
 }
