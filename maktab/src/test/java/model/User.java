64ec9f8fe928285677495dc41be56f0eebea16ca
abbas1381 <basjfryan@gmail.com>
2023-09-10 18:32:33 +0330

    add delete method

--- maktab/src/test/java/model/User.java
+++ maktab/src/test/java/model/User.java
@@ -1,18 +1,21 @@
 package model;
 
-public class user {
+public class User {
     private Integer id;
     private String firstname;
     private String lastname;
     private String username;
     private String password;
 
-    public user(Integer id, String firstname, String lastname, String username, String password) {
+    public User(Integer id, String firstname, String lastname, String username, String password) {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
         this.username = username;
         this.password = password;
+    }
+
+    public User(int id) {
     }
 
     public Integer getId() {
