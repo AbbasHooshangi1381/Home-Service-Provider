e785a61e4a52d80e1d0e23c2e161c11a31065908
abbas1381 <basjfryan@gmail.com>
2023-09-13 20:14:44 +0330

    add connection field to jdbcconnection class

--- writer_book/src/main/java/connection/Jdbcconnection.java
+++ writer_book/src/main/java/connection/Jdbcconnection.java
@@ -1,9 +1,14 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Jdbcconnection {
+    Connection connection = DriverManager.getConnection
+            ("jdbc:postgresql://localhost:5432/postgres","postgres","abbas1381.aa");
 
 
-
-
-
+    public Jdbcconnection() throws SQLException {
+    }
 }
