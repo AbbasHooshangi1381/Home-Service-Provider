d2484b2f78e0eda155afb6da47d25520d9079139
abbas1381 <basjfryan@gmail.com>
2023-09-13 15:58:08 +0330

    edit the class

--- maktabwriter/src/main/java/Repository/AuthorRepository.java
+++ maktabwriter/src/main/java/Repository/AuthorRepository.java
@@ -1,6 +1,31 @@
 package Repository;
+import  Connection.Jdbcconnection;
+import model.Author;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AuthorRepository {
- public int save()
+ Jdbcconnection jdbcconnection=new Jdbcconnection();
+ Connection connection=jdbcconnection.getConnection();
+
+ public AuthorRepository() throws SQLException {
+ }
+
+ public int save(Author author) throws SQLException {
+  String add="INSERT INTO authors(author_id,firstname,lastname,age)VALUES(?,?,?,?)";
+  PreparedStatement preparedStatement= connection.prepareStatement(add);
+  preparedStatement.setInt(1,author.getAuthorid());
+
+  preparedStatement.setString(2,author.getFirstname());
+  preparedStatement.setString(3,author.getLastname());
+  preparedStatement.setInt(4, author.getAge());
+
+  int result= preparedStatement.executeUpdate();
+  return result;
+
+
+ }
 
 }
