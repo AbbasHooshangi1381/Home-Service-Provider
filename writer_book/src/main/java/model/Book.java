cbdd695f3a8b69ab76e57fee06b8508d8ed6deb1
abbas1381 <basjfryan@gmail.com>
2023-09-13 20:34:15 +0330

    create Book class with attribute and getter setter method and constructor

---
+++ writer_book/src/main/java/model/Book.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Book {
+
+   private int bookId;
+   private String title;
+   private   int year;
+   private   Author author;
+
+    public Book(int bookId, String title, int year, Author author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.year = year;
+        this.author = author;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+}
