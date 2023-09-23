6434b79aaf95dfaa87df80ae1710b332ccd7b242
abbas1381 <basjfryan@gmail.com>
2023-09-23 14:05:37 +0330

    create interface userRepository and edit superwarning

--- ostad_tamrin/src/main/java/repositories/UserRepository.java
+++ ostad_tamrin/src/main/java/repositories/UserRepository.java
@@ -1,4 +1,21 @@
 package repositories;
 
+import domain.User;
+@SuppressWarnings("unused")
 public interface UserRepository {
+    User[] findall();
+
+    User findByIRd(Long id);
+
+    Void deleteById(Long id);
+
+    User save(User user);
+
+    User update(User user);
+
+    long count();
+
+
+
+
 }
