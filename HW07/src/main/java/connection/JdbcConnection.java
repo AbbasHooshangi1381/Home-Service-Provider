a86b47db75a40826709b7e04d6b51b3a19ae9d24
abbas1381 <basjfryan@gmail.com>
2023-09-24 14:45:49 +0330

    nothing

--- HW07/src/main/java/connection/JdbcConnection.java
+++ HW07/src/main/java/connection/JdbcConnection.java
@@ -7,11 +7,15 @@ import java.sql.SQLException;
 
 public class JdbcConnection {
 
- Connection connection= (Connection) DriverManager.getConnection(
-         "jdbc:postgresql://localhost:5432/postgres","postgres","abbas1381.aa"
- );
+  private Connection connection  = (Connection) DriverManager.getConnection(
+           "jdbc:postgresql://localhost:5432/postgres","postgres","abbas1381.aa"
+   );
+
 
 
  public JdbcConnection() throws SQLException {
+ }
+ public Connection getConnection(){
+  return connection;
  }
 }
