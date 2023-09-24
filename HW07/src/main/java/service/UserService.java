31d3bf5607bc01a4282c45efe8bfa68dca3c9139
abbas1381 <basjfryan@gmail.com>
2023-09-24 15:42:18 +0330

    add method login

--- HW07/src/main/java/service/UserService.java
+++ HW07/src/main/java/service/UserService.java
@@ -4,24 +4,37 @@ import repository.UserRepository;
 import role.User;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class UserService {
-    private final UserRepository userRepository=new UserRepository();
+    private final UserRepository userRepository = new UserRepository();
 
     public UserService() throws SQLException {
     }
 
     public void register() throws SQLException {
-        User user=new User(2,"abbas","abbas1381","basjfryan@gmail.com","aassdd");
-        int result=userRepository.save(user);
+        User user = new User(2, "abbas", "abbas1381", "basjfryan@gmail.com", "aassdd");
+        int result = userRepository.save(user);
 
-        if (result !=0){
-            System.out.println(user.getName()+"successfully added to data base");
-        }
-        else
+        if (result != 0) {
+            System.out.println(user.getName() + "successfully added to data base");
+        } else
             System.out.println(" wrong!");
     }
 
+    public void login() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("put your username");
+        String username = input.nextLine();
+        System.out.println("enter your password");
+        String password = input.nextLine();
 
 
+    User user = UserRepository.login(username);
+    if ((user != null)  && user.getPassword().equals(password) ){
+        System.out.println(" login successfully ");
+    }
+else
+        System.out.println("wrong!");
+}
 }
