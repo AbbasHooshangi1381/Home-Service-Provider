71f8c9f0cfc7df82995b4760b66a3c1d054f56fc
abbas1381 <basjfryan@gmail.com>
2023-09-24 12:34:47 +0330

    create Brand class and make getter and setter

---
+++ HW07/src/main/java/role/Brand.java
@@ -0,0 +1,52 @@
+package role;
+
+public class Brand {
+
+    private int id;
+    private String name;
+    private String website;
+    private String description;
+
+
+    public Brand() {
+    }
+
+    public Brand(int id, String name, String website, String description) {
+        this.id = id;
+        this.name = name;
+        this.website = website;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
