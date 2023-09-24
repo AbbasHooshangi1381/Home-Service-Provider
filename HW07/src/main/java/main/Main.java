9f34e78adb019cedc7ff0d5e7992f4aa0008b191
abbas1381 <basjfryan@gmail.com>
2023-09-24 14:45:28 +0330

    input entity and call method save

--- HW07/src/main/java/main/Main.java
+++ HW07/src/main/java/main/Main.java
@@ -1,7 +1,15 @@
 package main;
 
-public class Main {
-    public static void main(String[] args) {
+import repository.UserRepository;
+import role.User;
 
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        UserRepository userRepository=new UserRepository();
+        User user=new User(2,"abbas","abbas1381","basjfryan@gmail.com","aassdd");
+        userRepository.save(user);
     }
 }
--- HW07/src/main/java/repository/UserRepository.java
+++ HW07/src/main/java/repository/UserRepository.java
@@ -1,4 +1,27 @@
 package repository;
 
+import com.sun.jdi.connect.spi.Connection;
+import connection.JdbcConnection;
+import role.User;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserRepository {
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public UserRepository() throws SQLException {
+    }
+
+    public int save(User user) throws SQLException {
+        String add = "INSERT INTO user1(name,username,email,password)VALUES(?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getUsername());
+        preparedStatement.setString(3, user.getEmail());
+        preparedStatement.setString(4, user.getPassword());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
