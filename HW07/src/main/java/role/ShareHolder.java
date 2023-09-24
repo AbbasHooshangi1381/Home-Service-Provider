a73642621764a19e144741f2295220fa607e8580
abbas1381 <basjfryan@gmail.com>
2023-09-24 13:29:29 +0330

    create class and getter setter

---
+++ HW07/src/main/java/role/ShareHolder.java
@@ -0,0 +1,50 @@
+package role;
+@SuppressWarnings("unused")
+public class ShareHolder {
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private String nationalCode;
+
+    public ShareHolder() {
+    }
+
+    public ShareHolder(int id, String name, String phoneNumber, String nationalCode) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+}
