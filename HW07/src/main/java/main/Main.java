263810213de61578fda4a74aae1bb9f9f2a3fe8f
abbas1381 <basjfryan@gmail.com>
2023-09-25 14:45:18 +0330

    call edit method

--- HW07/src/main/java/main/Main.java
+++ HW07/src/main/java/main/Main.java
@@ -3,15 +3,19 @@ package main;
 import repository.UserRepository;
 import role.User;
 import service.UserService;
+import util.AppllicationContext;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserService userService=new UserService();
-        userService.register();
+     /*   UserRepository userRepository= AppllicationContext.getUserRepository();
+        UserService userService= AppllicationContext.getUserService();*/
 
+        UserService userService=new UserService();
+        userService.changeName();
         userService.login();
+        userService.register();
 
     }
 }
