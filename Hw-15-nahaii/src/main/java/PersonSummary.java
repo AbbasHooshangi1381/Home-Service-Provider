73d947f2818fd5d98a6cb3e69cfe938e1c509bbe
abbas1381 <basjfryan@gmail.com>
2023-11-07 18:01:09 +0330

    edit personsummery class

--- Hw-15-nahaii/src/main/java/PersonSummary.java
+++ Hw-15-nahaii/src/main/java/PersonSummary.java
@@ -1,11 +1,18 @@
 import java.util.Date;
-
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@ToString
 public class PersonSummary {
     private Integer id;
     private String firstName;
     private String lastName;
     private Integer age;
     private Date birthDate;
+    private String gender;
+
 
 
 
