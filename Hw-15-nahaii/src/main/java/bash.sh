4b217f0b9ae2b690c677ecdc31b0f2bcce71909d
abbas1381 <basjfryan@gmail.com>
2023-11-07 11:31:32 +0330

    add bash of question 2

---
+++ Hw-15-nahaii/src/main/java/bash.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+//question2_of_bash
+
+create_file(){
+  for (( i = 0; i < 5; i++ )); do
+      touch "file$i.txt"
+  done
+  echo "5 files created"
+}
+
+add_hello_word(){
+  for file in file*.txt; do
+    echo "Hello word">>"$file"
+    done
+    echo "added hello word to all files"
+}
+
+replace_bash_word(){
+    for file in file*.txt; do
+  sed -i 's/world/bash/g' "$file"
+  done
+  echo "replacing is complete"
+}
+
+
+option=("create file" "add Hello word" "replace bash word to world word " "nothing")
+
+select option in "${option[@]}"; do
+  case $option in
+  "create file")
+  create_file
+    ;;
+  "add 'hello word'")
+  add_hello_word
+;;
+
+"replace 'bash word'")
+replace_bash_word
+;;
+"nothing")
+break
+;;
+*)
+  echo "invalid!"
+  ;;
+
+  esac
+
+done
